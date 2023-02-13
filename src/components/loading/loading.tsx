af1c24b67759c5156a3547f90db88c4f35869c59
pyaref <arefty56gh@gmail.com>
2023-02-13 19:51:18 +0330

    Upload Files

---
+++ src/components/loading/loading.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../types/types';
+
+const YarnovinLoading: React.FC<YarnovinLoadingProps> = (Props) => {
+    return (<h3>Loading Bar</h3>);
+}
+
+export default YarnovinLoading;
---
+++ src/components/types/types.d.ts
@@ -0,0 +1,3 @@
+type YarnovinLoadingProps = { 
+    mode: "OffsetChar" | "OpacityLogo" | "CircleLogo";
+}
